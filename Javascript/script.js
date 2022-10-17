dfd3ac6d989f28f25575fb5cb8370299b11d4b40
erikvdwiel <9019165@student.zadkine.nl>
2022-10-17 11:33:07 +0200

    mapsjes

---
+++ Javascript/script.js
@@ -0,0 +1,11 @@
+function kleuraanpassen(){
+    alert("de kleur wordt aangepast.");
+    console.log("het werkt ook in de console!");
+    Document.getElementById("kleur").style.color = "blue";
+}
+
+
+function opdr63()
+{
+
+}

6824ba6910e05cf8ce71717f1cb07983e176c425
erikvdwiel <9019165@student.zadkine.nl>
2022-11-03 22:10:46 +0100

    6.4 gemaakt

---
+++ opdracht 6/opdracht 6.4/index.js
@@ -0,0 +1,21 @@
+function calculate(num, num2) {
+    let answer = num + num2;
+    console.log(answer)
+}
+
+calculate(8, 15);
+
+function multiplyByFive(num)  {
+
+    let answer = num * 5
+    console.log(answer)
+}
+
+multiplyByFive(4);
+
+function calculateMinutes(num, num2) {
+    let answer = num / num2
+    console.log(answer)
+}
+
+calculateMinutes(3600, 60)

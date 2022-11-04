29c3e420c3261a16ef962d326f299b9d8c4ef957
erikvdwiel <9019165@student.zadkine.nl>
2022-11-04 10:48:49 +0100

    opdracht 8.1

---
+++ opdracht 8/index.js
@@ -0,0 +1,58 @@
+function multiply() {
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    if (getal1 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    } 
+    else if (getal2 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    }
+    else {
+        var antwoord = getal1 * getal2;
+        document.getElementById("antwoord").innerHTML = antwoord;
+    }
+}
+function divide() {
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    if (getal1 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    } 
+    else if (getal2 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    }
+    else {
+        var antwoord = getal1 / getal2;
+        document.getElementById("antwoord").innerHTML = antwoord;
+    }
+}
+
+function add() {
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    if (getal1 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    } 
+    else if (getal2 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    }
+    else {
+        var antwoord = getal1 + getal2;
+        document.getElementById("antwoord").innerHTML = antwoord;
+    }
+}
+
+function minus() {
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    if (getal1 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    } 
+    else if (getal2 < 0) {
+        document.getElementById("antwoord").innerHTML = "Getal te laag";
+    }
+    else {
+        var antwoord = getal1 - getal2;
+        document.getElementById("antwoord").innerHTML = antwoord;
+    }
+}
